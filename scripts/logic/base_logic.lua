4f5d30ac700d51c1ddffc237f7cd859445211d48
fedjoyd5 <fedjoyd5.fedmail@gmail.com>
2026-08-28 04:11:13 +0200

    [dev] pack work upload

---
+++ scripts/logic/base_logic.lua
@@ -0,0 +1,21 @@
+
+-- this is the file to put all your custom logic functions into.
+-- if you dont want to use the json based logic you can switch to a graph-based logic method.
+-- the needed functions for that are in `/scripts/logic/graph_logic/logic_main.lua`.
+
+-- ^$CanAccessNightmare
+function CanAccessNightmare()
+    return 5;
+end
+
+-- ^$CanAccessHell
+function CanAccessHell()
+    return 5;
+end
+
+-- function <name> (<parameters if needed>)
+--     <actual code>
+--     <indentations are just for readability>
+-- end
+--
+                
