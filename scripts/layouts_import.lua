12b255caa57d90d70fbc1d3640170f285b8fc9e5
fedjoyd5 <fedjoyd5.fedmail@gmail.com>
2026-09-25 02:23:29 +0200

    [dev] Add zone key to the UI and Add Broadcast display choice

--- scripts/layouts_import.lua
+++ scripts/layouts_import.lua
@@ -3,8 +3,16 @@ Tracker:AddLayouts("layouts/settings_popup.json")
 
 --Tracker:AddLayouts("layouts/items.json")
 Tracker:AddLayouts("layouts/gate_key.json")
+Tracker:AddLayouts("layouts/zone_key.json")
 Tracker:AddLayouts("layouts/skills.json")
 
 Tracker:AddLayouts("layouts/tabs.json")
 Tracker:AddLayouts("layouts/tracker.json")
-Tracker:AddLayouts("layouts/broadcast.json")
+
+if (Tracker.ActiveVariantUID == "Map Tracker FB") then
+    Tracker:AddLayouts("layouts/broadcast_FULL.json")
+elseif (Tracker.ActiveVariantUID == "Map Tracker KB") then
+    Tracker:AddLayouts("layouts/broadcast_KEY.json")
+elseif (Tracker.ActiveVariantUID == "Map Tracker SB") then
+    Tracker:AddLayouts("layouts/broadcast_SKILL.json")
+end
